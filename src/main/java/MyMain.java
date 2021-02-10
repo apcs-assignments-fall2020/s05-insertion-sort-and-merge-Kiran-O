d818fbc941c81b6bd3a734b087c9931b94f1aec5
Kiran-O <22kokelly@athenian.org>
2021-02-10 03:08:38 +0000

    Passed all tests

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,19 +1,54 @@
 import java.util.ArrayList;
-
+import java.util.Arrays;
 public class MyMain {
 
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+        for (int k = 1; k < list.size(); k++){
+            int ynk = list.get(k);
+            int m = k - 1;
+            while ((m > - 1) && (list.get(m) > ynk)){
+                list.set(m + 1, list.get(m));
+                m--;
+
+
+            }
+            list.set(m + 1, ynk);
+        }
+        return list;
     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int [] jkl = new int[arr1.length + arr2.length];
+        int k = 0;
+        int m = 0;
+        int i = 0;
+
+        while (k < arr1.length && m < arr2.length){
+            if (arr1[k] < arr2[m]){
+                jkl[i] = arr1[k];
+                k++;
+            }
+            else {
+                jkl[i] = arr2[m];
+                m++;
+            }
+            i++;
+        }
+        while (k < arr1.length){
+            jkl[i] = arr1[k];
+            k++;
+            i++;
+        }
+        while (m < arr2.length){
+            jkl[i] = arr2[m];
+            m++;
+            i++;
+        }
+        return jkl;
     }
 
     public static void main(String[] args) {
